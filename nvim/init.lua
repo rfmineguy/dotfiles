90a828724a4bef1a13f1533f015d8bccebfc24e8
Riley Fischer <rfmineguy@gmail.com>
2025-09-02 15:36:26 -0700

    reduce pluggin count of neovim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,7 +9,7 @@ vim.opt.wrap = true
 vim.opt.breakindent = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.expandtab = false
+vim.opt.expandtab = true
 vim.opt.relativenumber = true
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
@@ -67,29 +67,12 @@ print(lazy.path)
 lazy.opts = {}
 lazy.setup({
 	-- Plugins here
-	-- { 'christoomey/vim-tmux-navigator' },
-	{ 'folke/tokyonight.nvim' },
-	{
-		"https://git.sr.ht/~swaits/scratch.nvim",
-		lazy = true,
-		keys = {
-			{ "<leader>bs", "<cmd>Scratch<cr>", desc = "Scratch Buffer", mode = "n" },
-			{ "<leader>bS", "<cmd>ScratchSplit<cr>", desc = "Scratch Buffer (split)", mode = "n" },
-		},
-		cmd = {
-			"Scratch",
-			"ScratchSplit",
-		},
-		opts = {},
-	},
 	{ 'nvim-lua/plenary.nvim' },
 	{ 'LinArcX/telescope-command-palette.nvim' },
 -- or                              , branch = '0.1.x','}
 	{ 'lukas-reineke/indent-blankline.nvim', main = 'ibl', opts = {} },
 	{ 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
 	{ 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
-	{ 'nvim-treesitter/playground' },
-	{ 'tpope/vim-fugitive' },
 	-- LSP stuff
   { 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x' },
   { 'neovim/nvim-lspconfig' },
@@ -97,28 +80,18 @@ lazy.setup({
   { 'hrsh7th/nvim-cmp', event = { "InsertEnter", "CmdlineEnter" } },
 	{ 'williamboman/mason.nvim' },
 	{ 'williamboman/mason-lspconfig.nvim' },
-	{ 'Civitasv/cmake-tools.nvim' },
-	{ 'L3MON4D3/LuaSnip', version = "v2.*", build = "make install_jsregexp", dependencies = { "rafamadriz/friendly-snippets" }},
-	-- Git
-	{ 'NeogitOrg/neogit', dependencies = { "sindrets/diffview.nvim", "ibhagwan/fzf-lua" }, config = true },
-	-- Haskell
-	{
-		'mrcjkb/haskell-tools.nvim',
-		version = '^3', -- Recommended
-		ft = { 'haskell', 'lhaskell', 'cabal', 'cabalproject' },
-	},
-	{ 'saadparwaiz1/cmp_luasnip' },
-	{ 'nvim-telescope/telescope.nvim', tag = '0.1.5' },
-	{ 'nvim-telescope/telescope-dap.nvim' },
-	{ "folke/neodev.nvim", opts = {},
-		config = function()
-			require("neodev").setup({
-				library = { plugins = { "nvim-dap-ui" }, types = true }
-			})
-		end
-	},
-	{ 'ryanoasis/vim-devicons' }
-	-- { 'theprimeagen/harpoon' }
+
+  { 'lewis6991/gitsigns.nvim' },
+  { 'nvim-telescope/telescope.nvim', tag = '0.1.5' },
+  {
+    "vimwiki/vimwiki",
+    init = function()
+      vim.g.vimwiki_list = {
+        { path = '~/vimwiki_2025/docs', syntax = 'markdown', ext = '.md' },
+      }
+      vim.vimwiki_global_ext = 0
+    end,
+  },
 })
 
 vim.opt.termguicolors = true
